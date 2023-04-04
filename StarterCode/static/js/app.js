1adba27fa0cae3016a64e34928c7f320439819a3
leahkrause <leahrkrause@gmail.com>
2023-04-04 16:45:03 -0700

    redoing js file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,97 +1,135 @@
-// Start by setting the desired url to a variable
+// Define the URL to fetch the JSON data
 let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+// Function to display demographic information of a test subject
+function displayDemographicInfo(sample) {
+  // Fetch data from the JSON URL
+  d3.json(url).then((data) => {
+    // Extract the metadata array from the fetched data
+    let metadata = data.metadata;
+    // Filter metadata for the selected test subject
+    let resultArray = metadata.filter((sampleObj) => sampleObj.id == sample);
+    // Get the first (and only) object in the filtered array
+    let result = resultArray[0];
+
+    // Select the panel where demographic information will be displayed
+    let panel = d3.select("#sample-metadata");
+    // Clear the existing demographic information
+    panel.html("");
+
+    // Append the key-value pairs of the demographic information to the panel
+    Object.entries(result).forEach(([key, value]) => {
+      panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+    });
+  });
+}
+
+// Function to initialize the dashboard
 function init() {
+  // Select the dropdown menu
+  var dropdown = d3.select("#selDataset");
 
-    // First select dropdown menu 
-    var dropdown = d3.select("#selDataset");
+  // Fetch data from the JSON URL
+  d3.json(url).then((data) => {
+    // Extract the names array from the fetched data
+    let names = data.names;
 
-    // Fetch JSON data and read in using D3
-    d3.json(url).then((data)=> {
-    console.log(data);
-    
-    // Add names to a variable 
-    names = data.names;
-
-    console.log(names);
-
-    // Loop through names and append them to the dropdown
-    for (let i = 0; i < names.length; i++){
-        dropdown.append("option").property("value", names[i]).text(names[i]);
-        console.log(names[i]);
-    };
-
-    dropdown.on("onChange", function(){
-        var choice = dropdown.property("value");
-        // console.log(d3.event.target);
-        console.log(choice);
+    // Append each name in the names array to the dropdown menu
+    names.forEach((name) => {
+      dropdown.append("option").property("value", name).text(name);
     });
 
-    // Use the first sample from the list to build the initial plots
+    // Get the first test subject's ID from the names array
     let firstSample = names[0];
-    create_chart(firstSample);
-    console.log(create_chart);
-    d3.selectAll(dropdown).on("change", updatePlotly);
+    // Create the charts for the first test subject
+    create_charts(firstSample);
+    // Display the demographic information for the first test subject
+    displayDemographicInfo(firstSample);
+  });
+}
 
-})};
+// Function to handle the change event of the dropdown menu
+function optionChanged(newSample) {
+  // Create the charts for the newly selected test subject
+  create_charts(newSample);
+  // Display the demographic information for the newly selected test subject
+  displayDemographicInfo(newSample);
+}
 
-
-// Create chart function
-function create_chart(sample) {
-
-    d3.json(url).then((data)=> {
-        
-    // Sort the data by OTUs 
-    // let sortedByOTUs = data.sort((a, b) => b.sample_values - a.sample_values);
-    
-    // Reverse the array to accommodate Plotly's defaults
-    // reversedData = slicedData.reverse();
-
-    // 
+// Function to create charts for a test subject
+function create_charts(sample) {
+  // Fetch data from the JSON URL
+  d3.json(url).then((data) => {
+    // Extract the samples array from the fetched data
     let samples = data.samples;
-    let resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+    // Filter samples for the selected test subject
+    let resultArray = samples.filter((sampleObj) => sampleObj.id == sample);
+    // Get the first (and only) object in the filtered array
     let result = resultArray[0];
-    console.log(result)
 
+    // Get the top 10 OTU IDs, labels, and values for the selected test subject
     let otu_ids = result.otu_ids.slice(0, 10).reverse();
     let otu_labels = result.otu_labels.slice(0, 10).reverse();
     let sample_values = result.sample_values.slice(0, 10).reverse();
 
-    let plot_data = [
-        {
+    // Set up bar chart data
+    let bar_data = [
+      {
         x: sample_values,
-        y: otu_ids,
+        y: otu_ids.map((otu_id) => `OTU ${otu_id}`),
         text: otu_labels,
         type: "bar",
-        orientation: "h"
-        }
+        orientation: "h",
+      },
     ];
 
-    Plotly.newPlot("bar", plot_data);
 
-})};
+    // Plot the bar chart
+    Plotly.newPlot("bar", bar_data);
 
-// Update chart function
-function updatePlotly() {
+// chart data
 
-    let dropdo
+let filteredArray = data.metadata.filter((obj) => obj.id == sample);
+let filter = filteredArray[0];
 
+let gauge_data = [
+  {
+    domain: { x: [0, 1], y: [0, 1] },
+    value: filter.wfreq,
+    title: { text: "Belly Button Washing Frequency" },
+    type: "indicator",
+    mode: "gauge+number",
+    gauge: { axis: { range: [null, 9] } },
+  },
+];
 
-// }
-// choices.on("change", function(){
-//     var choice = choices.property("value");
-//     console.log(choice)});
+// Plot the gauge chart
+Plotly.newPlot("gauge", gauge_data);
 
+// Set up bubble chart data
+let bubble_data = [
+  {
+    x: result.otu_ids,
+    y: result.sample_values,
+    text: result.otu_labels,
+    mode: "markers",
+    marker: {
+      size: result.sample_values,
+      color: result.otu_ids,
+      colorscale: "Earth",
+    },
+  },
+];
 
+// Set up bubble chart layout
+let bubble_layout = {
+  xaxis: { title: "OTU ID" },
+  hovermode: "closest",
+};
 
-// Call the init function
+// Plot the bubble chart
+Plotly.newPlot("bubble", bubble_data, bubble_layout);
+  });
+}
+
 init();
-
-
-
-//     console.log(data[choice])});
-
-// function create_plot
-
-// create_plot(plot_data["OTUs"])
-
